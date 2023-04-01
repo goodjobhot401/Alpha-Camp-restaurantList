08a30606ad145f344fe598035f146a2da28ee17b
goodjobhot401 <goodjobhot@gmail.com>
2023-04-01 17:30:10 +0800

    feat: add sort function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -2,28 +2,57 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
-
 // search
-router.get('/search', (req, res) => {
+router.get('/', (req, res) => {
   if (!req.query.keyword) {
     return res.redirect('/')
   }
-  const keyword = req.query.keyword.toLowerCase().trim()
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const sortMethod = req.query.sort
+  let sortKeyword = { name: 'asc' }
+
+  if (sortMethod === 'Z > A')
+    sortKeyword = { name: 'desc' }
+
+  if (sortMethod === '類別')
+    sortKeyword = { category: 'asc' }
+
+  if (sortMethod === '地區')
+    sortKeyword = { location: 'asc' }
+
+
   return Restaurant.find({})
     .lean()
-    .then(() => console.log(keyword))
-    .then((restaurants) => {
-      const results = restaurants.filter((restaurant) => {
-        return restaurant.name.toLowerCase().includes(keyword) ||
-          restaurant.category.toLowerCase().includes(keyword) ||
-          restaurant.name_en.toLowerCase().includes(keyword)
-      })
-      res.render('index', { restaurants: results, keyword })
+    .sort(sortKeyword)
+    .then(restaurants => {
+      const results = restaurants.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword) ||
+          data.name_en.toLowerCase().includes(keyword)
+      )
+      res.render('index', { restaurant: results, keyword, sortMethod })
     })
     .catch(err => {
       console.log(err)
       res.render('errorPage', { error: err.message })
     })
+
+  // return Restaurant.find({})
+  //   .lean()
+  //   .then(() => console.log(keyword))
+  //   .then((restaurants) => {
+  //     const results = restaurants.filter((restaurant) => {
+  //       return restaurant.name.toLowerCase().includes(keyword) ||
+  //         restaurant.category.toLowerCase().includes(keyword) ||
+  //         restaurant.name_en.toLowerCase().includes(keyword)
+  //     })
+  //     res.render('index', { restaurants: results, keyword })
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //     res.render('errorPage', { error: err.message })
+  //   })
 })
 
 module.exports = router
