4499273e44667507239877b855178d0186673caf
goodjobhot401 <goodjobhot@gmail.com>
2023-04-27 15:40:50 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,4 +6,15 @@ router.get('/login', (req, res) => {
 })
 
 
+router.post('/login', (req, res) => {
+
+})
+
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
+
 module.exports = router
